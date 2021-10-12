cb7fa88672c6258eff3416b8e07f780bd58e03ca
seol <rrrreeees894@gmail.com>
2021-10-12 16:14:05 +0900

    Layout

---
+++ src/pages/Detail.js
@@ -0,0 +1,51 @@
+/* eslint-disable */
+
+import React, {useState} from "react";
+import { useEffect } from "react";
+import { useHistory, useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import Grid from "../elements/Grid";
+import './Detail.scss';
+
+function Detail(props) {
+
+ 
+  let [inputData, inputData변경] = useState('');
+
+    let { id } = useParams();
+    //뒤로가기 1(2)
+    let history = useHistory();
+    
+
+    return (
+        <div className="container">
+          <박스>
+            <제목 className="red">제품 후기</제목>
+          </박스>
+   
+          <Grid is_flex>
+            <Grid>
+              <img src="https://imgc.1300k.com/aaaaaib/goods/215024/83/215024836202.jpg?3" width="100%" />
+            </Grid>
+            <Grid padding="16px">
+              <h4>장난감 1</h4>
+              <p>닉네임</p>
+              <p>저희 고양이가 너무 좋아해서 후기글 남깁니다 매일 매일 가지고 놀아요~~~</p>
+            </Grid>
+          </Grid>
+        </div>
+    )
+}
+
+
+let 박스 = styled.div`
+  padding: 30px;
+`;
+
+let 제목 = styled.h4`
+  font-size: 25px;
+  color: ${props => props.색상}
+`;
+
+
+export default Detail;
--- src/pages/PostDetail.js
+++ src/pages/PostDetail.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React  from 'react';
+import { Grid, Comments, Detail } from './index';
 
-const PostDetail = () => {
+const PostDetail = (props) => {
+    
     return (
-        <div>
-            상세페이지
-        </div>
+        <React.Fragment>
+            <Grid padding="16px">
+                {/* 상세페이지 카드 */}
+                <Grid is_flex>
+                    <Detail />
+                </Grid>
+
+                <hr style={{
+                    height: '2px'}}/>
+                <Grid>
+                    <Comments/>
+                </Grid>
+            </Grid>
+        </React.Fragment>
     );
 };
 
